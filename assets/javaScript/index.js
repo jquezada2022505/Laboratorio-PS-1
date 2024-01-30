cc6c6cc73debc80a828b01d2fe6756572aa584e4
jquezada2022505 <jquezada-2022505@kinal.edu.gt>
2024-01-29 21:56:07 -0600

    Se creo JS del Index

---
+++ assets/javaScript/index.js
@@ -0,0 +1,26 @@
+var usuarios = [
+    { nombreUsuario: "admin", password: "123" },
+    { nombreUsuario: "user", password: "123" },
+];
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var nombreUsuario = document.getElementById('nombreUsuario').value;
+    var password = document.getElementById('password').value;
+
+    var isValid = usuarios.some(function(user) {
+        return user.nombreUsuario === nombreUsuario && user.password === password;
+    });
+
+    document.getElementById("nombreUsuario").value = "";
+    document.getElementById("password").value = "";
+
+    if (isValid) {
+        alert("BIENVENIDO AL SISTEMA");
+        window.location.href = "home.html";
+    } else {
+        alert("Contraseña incorrecta, intente de nuevo");
+
+    }
+});
